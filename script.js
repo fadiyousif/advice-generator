43c28a601adba7830d710b158327fac40ffa09cb
Fadi <fadi.yousif1x@gmail.com>
2022-11-06 01:07:31 +0100

    fix indentation

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-const adviceId = document.getElementById('advice-id');
-const adviceText = document.getElementById('advice-text');
-const diceButton = document.getElementById('dice-button');
+const adviceId = document.getElementById("advice-id");
+const adviceText = document.getElementById("advice-text");
+const diceButton = document.getElementById("dice-button");
 
 // #146 and #76 contain weird characters
 const adviceToSkip = [146, 76];
 const adviceRendered = [];
 
 const fetchAdvice = () => {
-    fetch(`https://api.adviceslip.com/advice`)
-    .then(res => res.json())
-    .then(adviceObj => renderAdvice(adviceObj))
-    .catch(err => console.error(err))
-}
+   fetch(`https://api.adviceslip.com/advice`)
+      .then((res) => res.json())
+      .then((adviceObj) => renderAdvice(adviceObj))
+      .catch((err) => console.error(err));
+};
 
 const renderAdvice = ({ slip }) => {
-    const { advice, id } = slip;
+   const { advice, id } = slip;
 
-    if (adviceRendered.includes(id) || adviceToSkip.includes(id)) {
-        fetchAdvice();
-        return;
-    }
+   if (adviceRendered.includes(id) || adviceToSkip.includes(id)) {
+      fetchAdvice();
+      return;
+   }
 
-    adviceId.innerText = `Advice #${id}`;
-    adviceText.innerText = `“${advice}”`;
+   adviceId.innerText = `Advice #${id}`;
+   adviceText.innerText = `“${advice}”`;
 
-    adviceRendered.push(id);
-}
+   adviceRendered.push(id);
+};
 
-diceButton.addEventListener('click', fetchAdvice);
-window.addEventListener('DOMContentLoaded', fetchAdvice);
+diceButton.addEventListener("click", fetchAdvice);
+window.addEventListener("DOMContentLoaded", fetchAdvice);
